72d28eeacd9aacc46676bb0967c4890994dbba72
Emmanuel Pilande <epilande@gmail.com>
2016-11-29 21:04:41 -1000

    🔨 Export function to retrieve brightness

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const robot = require('robotjs');
 const tinycolor = require('tinycolor2');
-const { width, height } = robot.getScreenSize();
-const columns = 8;
-const points = 32;
-const columnSize = width / columns;
-const pointsPerColumn = Math.floor(points / columns);
-const pointDistance = height / pointsPerColumn;
 
-let screenBrightness = 0;
-let currentColumn = -1;
-let currentRow = 0;
+module.exports = () => {
+  const { width, height } = robot.getScreenSize();
+  const columns = 8;
+  const points = 32;
+  const columnSize = width / columns;
+  const pointsPerColumn = Math.floor(points / columns);
+  const pointDistance = height / pointsPerColumn;
 
-for (let i = 0; i < points; i++) {
-  if (i % pointsPerColumn === 0) {
-    currentRow = 0;
-    currentColumn++;
-  } else {
-    currentRow++;
+  let screenBrightness = 0;
+  let currentColumn = -1;
+  let currentRow = 0;
+
+  for (let i = 0; i < points; i++) {
+    if (i % pointsPerColumn === 0) {
+      currentRow = 0;
+      currentColumn++;
+    } else {
+      currentRow++;
+    }
+
+    const sample = robot.getPixelColor(
+      (columnSize * currentColumn) + (columnSize / 2),
+      (pointDistance * currentRow) + (pointDistance / 2)
+    );
+
+    const brightness = tinycolor(sample).getBrightness();
+
+    screenBrightness += brightness;
   }
 
-  const sample = robot.getPixelColor(
-    (columnSize * currentColumn) + (columnSize / 2),
-    (pointDistance * currentRow) + (pointDistance / 2)
-  );
-
-  const brightness = tinycolor(sample).getBrightness();
-
-  screenBrightness += brightness;
-}
-
-const averageBrightness = screenBrightness / points;
-
-console.log(averageBrightness);
+  return screenBrightness / points;
+};
